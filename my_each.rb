1b310deeba18fae0a6558d970c22603127cf13eb
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-21 04:18:09 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(collection)
   counter = 0
   while counter < collection.length
     yield(collection[counter])
-    puts "#{collection[counter]}"
+    collection[counter]
     counter +=1
   end
 end
